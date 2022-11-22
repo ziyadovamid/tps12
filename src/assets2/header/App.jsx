9329532df318a005cfe5711f5f7db127bd512002
ziyadovamid <114071715+ziyadovamid@users.noreply.github.com>
2022-11-22 03:14:08 -0800

    Add files via upload

---
+++ src/assets2/header/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div class="bot">
+      <h3>Ziyadov Amid</h3>
+      <p>Now I can create component</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './assets2/header/App'
+import "./index.css";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App/>
+)
